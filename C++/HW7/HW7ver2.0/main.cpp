56cb738a335f5181b553cde52c7b839d3111fc33
ghk0914 <82981454+ghk0914@users.noreply.github.com>
2022-10-29 15:38:43 +0800

    Add files via upload

--- C++/HW7/HW7ver2.0/main.cpp
+++ C++/HW7/HW7ver2.0/main.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;;
-int a,b,c;
-int hcf1=1,hcf2=1,lcm1=1,lcm2=1;
-int main(int argc, char** argv) {
-    for(int k=0;k<3;k++){
-        cin>>a>>b>>c;
-        for(int i=1;i<101;i++){
-            if(a%i==0 and b%i==0){
-            hcf1=i;
-            }
+int hcf_maker(int a,int b){
+    int hcf;
+    for(int i=1;i<101;i++){
+        if(a%i==0 and b%i==0){
+            hcf=i;
         }
-    lcm1=a*b/hcf1;
-        for(int j=1;j<101;j++){
-            if(lcm1%j==0 and c%j==0){
-            hcf2=j;
-            }
-        }
-    lcm2=lcm1*c/hcf2;
-    cout<<lcm2<<endl;
-
     }
-    
+    return hcf;
+}
+int lcm_maker(int a,int b,int hcf){
+    int lcm=1;
+    lcm=a*b/hcf;
+    return lcm;
+}
+
+int main(int argc, char** argv) {
+    for(int i=0;i<3;i++){
+    int a,b,c;
+    int hcf1=1,hcf2=1,lcm1=1,lcm2=1;
+    cin>>a>>b>>c;
+    hcf1=hcf_maker(a,b);
+    lcm1=lcm_maker(a,b,hcf1);
+    hcf2=hcf_maker(lcm1,c);
+    lcm2=lcm_maker(lcm1,c,hcf2);
+    cout<<lcm2<<endl;
+    }
+    system("pause");
 	return 0;
 }
